6233d4a68ed0eb06fada1a45a349dc89fcccf2f1
Sah <rasah@softwareag.com>
2024-03-29 11:18:01 +0530

    set up index page

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module rest-basics
 go 1.18
 
 require github.com/joho/godotenv v1.5.1
-
-require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,78 +1,29 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load(".env")
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("PORT not set in the environment variables!")
-	}
-
-	router := mux.NewRouter()
-
-	// index route
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		fmt.Println("Request Method:", r.Method)
-		fmt.Println("Request URL:", r.URL.String())
-		fmt.Println("Request Headers:")
-		for name, values := range r.Header {
-			for _, value := range values {
-				fmt.Printf("%s: %s\n", name, value)
-			}
-		}
-		fmt.Println("Request body\n", r.Body)
-
-		message := map[string]interface{}{
-			"message": "Welcome to the index page",
-			"status":  200,
-			"success": true,
-		}
-		jsonResponse, err := json.Marshal(message)
-		if err != nil {
-			http.Error(w, "An error occured parsing JSON!", http.StatusInternalServerError)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonResponse)
-	})
-
-	// profiles route
-	router.HandleFunc("/profiles", addItem).Methods("POST")
+	PORT := os.Getenv("PORT")
 
 	server := &http.Server{
-		Handler: router,
-		Addr:    ":" + port,
+		Addr:    ":" + PORT,
+		Handler: http.HandlerFunc(basicHandler),
 	}
-	log.Printf("Server running on the port : %v", port)
+
 	err := server.ListenAndServe()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("There was an error running the server")
 	}
-
 }
 
-type User struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-}
-
-type Profile struct {
-	Department  string `json:"department"`
-	Designation string `json:"designation"`
-	Employee    User   `json:"employee"`
-}
-
-func addItem(q http.ResponseWriter, r *http.Request) {
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the index page!"))
 
 }
