d7debc33b71cb7c5f233c1184b2310b2e37344d1
krahul2024 <sandipkumar2024@gmail.com>
2024-03-29 22:30:12 +0530

    Built basic server without use of any frameworks like chi or gorilla mux, added http method handling!

--- main.go
+++ main.go
@@ -1,26 +1,73 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", indexFunction)
+	// mux.HandleFunc("/", indexHandler)
+	// mux.HandleFunc("/about", aboutHandler)
+	// mux.HandleFunc("/post", postHandler)
 
-	log.Fatal(http.ListenAndServe(":3300", nil))
+	mux.HandleFunc("/", handleRequest(indexHandler, http.MethodGet, http.MethodPost))
+	mux.HandleFunc("/about", handleRequest(aboutHandler, http.MethodGet))
+	mux.HandleFunc("/post", handleRequest(postHandler, http.MethodPost))
+
+	server := &http.Server{
+		Addr:    ":3300",
+		Handler: mux,
+	}
+
+	log.Printf("Starting the server at PORT : 3300\n")
+
+	server.ListenAndServe()
 }
 
-func indexFunction(res http.ResponseWriter, req *http.Request) {
+func indexHandler(res http.ResponseWriter, req *http.Request) {
 	data, err := io.ReadAll(req.Body)
 	if err != nil {
-		http.Error(res, "An error occured", http.StatusBadRequest)
+		http.Error(res, "An error occured!\n", http.StatusBadRequest)
 		return
 	}
-	log.Printf("%s", data)
-	res.Write([]byte("This is index page..." + string(data)))
+	res.Write([]byte("This is the index route!\n" + string(data)))
 }
+
+func aboutHandler(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("This is the about route!\n"))
+}
+
+func postHandler(res http.ResponseWriter, req *http.Request) {
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, "An error occured!\n", http.StatusBadRequest)
+		return
+	}
+	res.Write([]byte("This is the post route!\n" + string(data)))
+}
+
+/* This function removes the repetitive task of checking method type in each of the xhandler
+functions, otherwise we need to check what is the request method type and then perform
+the action accordingly.
+*/
+
+func handleRequest(handler http.HandlerFunc, allowedMethods ...string) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		for _, method := range allowedMethods {
+			if req.Method == method {
+				handler(res, req)
+				return
+			}
+		}
+	}
+}
+
+// Task to complete
+/*
+1. Perform actual get and post methods using this approach
+2. Perform database related operations using this approach
+3. Perform authentication and cookie setting using this approach
+*/
